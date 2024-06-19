d47e6fe9b76e910680cf99ee55c5e293a813d8e4
majidkawouk <57314886+majidkawouk@users.noreply.github.com>
2024-06-19 17:15:34 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 // src/app/page.js
-import Navbar from "../components/Navbar";
-import MainContent from "../components/MainContent";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
 
 export default function Home() {
   return (
